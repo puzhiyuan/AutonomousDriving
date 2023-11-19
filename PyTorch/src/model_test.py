a10e706855ec76db8925ed45c48836ee56673f2e
puzhiyuan <puzhiyuan185489643@gmail.com>
2023-11-19 16:57:16 +0800

    modified the AlexNet and model_test

--- PyTorch/src/model_test.py
+++ PyTorch/src/model_test.py
@@ -2,7 +2,7 @@
 Author: ‘puzhiyuan’ ‘puzhiyuan185489643@gmail.com’
 Date: 2023-11-14 20:10:33
 LastEditors: ‘puzhiyuan’ ‘puzhiyuan185489643@gmail.com’
-LastEditTime: 2023-11-15 21:45:39
+LastEditTime: 2023-11-19 16:54:28
 FilePath: \AutonomousDriving\PyTorch\src\model_test.py
 Description: test code
 '''
@@ -13,12 +13,13 @@ import torch
 def model_test(model, dataloader, device):
     model.eval()
     correct = 0
-    total = len(dataloader)
+    total = 0
     for data in dataloader:
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = model(inputs)
         _, predict = torch.max(outputs.data, 1)
+        total += labels.size(0)
         correct += (predict == labels).sum().item()
     return correct / total
 
